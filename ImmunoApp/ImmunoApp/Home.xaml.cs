a3ca1b85d55d680c365f207128744b5de41d0669
Gießer <christian.giesser@student.uni-siegen.de>
2018-04-27 13:17:41 +0200

    Initial Version
    Erster Upload

---
+++ ImmunoApp/ImmunoApp/Home.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ImmunoApp
+{
+    public partial class Home : ContentPage
+    {
+        public Home()
+        {
+            InitializeComponent();
+
+            taking.Image = (Xamarin.Forms.FileImageSource)ImageSource.FromFile("ImmunoApp.Images.taking.png");
+        }
+    }
+}
---
+++ ImmunoApp/ImmunoApp/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ImmunoApp
+{
+	public partial class MainPage : ContentPage
+    {
+		public MainPage()
+		{ 
+			InitializeComponent();
+            image.Source = ImageSource.FromResource("ImmunoApp.Images.Logo.jpg");
+            image.GestureRecognizers.Add(new TapGestureRecognizer());
+		}
+
+      
+        //Login
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new Home());
+        }
+        //Passwort vergessen
+        void Handle_Clicked_1(object sender, System.EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
